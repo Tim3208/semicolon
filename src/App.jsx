2590d94ca7e87b5daba12fc043fd32dea0fb75d6
Tim3208 <joungou.park@gmail.com>
2025-08-28 00:34:55 +0900

    chore: 초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* 메인 */}
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+
+function MainPage() {
+  return (
+    <div className="MainPage">
+      <h1>Main Page</h1>
+    </div>
+  );
+}
+export default MainPage;
